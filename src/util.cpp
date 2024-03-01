070a28b36e40b130b4ddeff7ead3b59d71233847
SomeItalianGuy <vivamork@gmail.com>
2024-03-01 14:03:10 +0100

    Implemented the degsToRads function

--- src/util.cpp
+++ src/util.cpp
@@ -1,1 +1,6 @@
 #include "util.hpp"
+
+
+float util::degsToRads(float angleInDegs) {
+    return (angleInDegs / 180 * M_PI);
+}
--- src/util.hpp
+++ src/util.hpp
@@ -1,4 +1,10 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <cmath>
+
+namespace util {
+    float degsToRads(float angleInDegs); 
+}
+
 #endif
