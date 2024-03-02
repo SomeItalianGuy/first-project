520da8e3d83f719463313f7605aed0b3d69273f1
SomeItalianGuy <vivamork@gmail.com>
2024-03-02 10:07:42 +0100

    Implemented the inheritance hierarchy

--- src/eventListener.cpp
+++ src/eventListener.cpp
@@ -1,1 +1,7 @@
 #include "eventListener.hpp"
+
+// Constructor / Destructor
+
+EventListener::EventListener() {}
+
+EventListener::~EventListener() { delete this->m_shape; }
--- src/eventListener.hpp
+++ src/eventListener.hpp
@@ -1,4 +1,19 @@
 #ifndef EVENTLISTENER_HPP
 #define EVENTLISTENER_HPP
 
+#include <SFML/Graphics.hpp>
+
+class EventListener {
+ private:
+  //  Nothing private
+ public:
+  sf::CircleShape* m_shape;
+  virtual void consumeEvent(sf::Event&) = 0;
+
+  // Constructor / Destructor
+
+  EventListener();
+  virtual ~EventListener();
+};
+
 #endif
