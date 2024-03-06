0f5df37ed0a8aec19ad70347ec7625d7e12f85b4
SomeItalianGuy <vivamork@gmail.com>
2024-03-06 07:54:48 +0100

    Changed the vector class name to GenericVector, Added the declaration for the modulus method

--- src/vector.hpp
+++ src/vector.hpp
@@ -7,7 +7,7 @@
 namespace logic{
 
 template <class T, std::size_t size>
-class Vector{ 
+class GenericVector{ 
 
     static_assert(size > 0);
     static_assert(std::is_arithmetic<T>::value);
@@ -17,27 +17,28 @@ class Vector{
     public:
     // Constructor / Destructor
     
-    Vector();
-    Vector(Vector const& other);
+    GenericVector();
+    GenericVector(GenericVector const& other);
 
     // Public methods
 
+    T modulus();
 
     // Operators
 
-    Vector<T, size> operator+(Vector<T, size> const& other);
-    Vector<T, size> operator-(Vector<T, size> const& other);
+    GenericVector<T, size> operator+(GenericVector<T, size> const& other);
+    GenericVector<T, size> operator-(GenericVector<T, size> const& other);
     template<class Scalar, class = std::enable_if_t<std::is_arithmetic<Scalar>>>
-    Vector<T, size> operator*(Vector<T, size> const& other);
+    GenericVector<T, size> operator*(GenericVector<T, size> const& other);
 
     template<class Scalar, class = std::enable_if_t<std::is_arithmetic<Scalar>>>
-    Vector<T, size> operator/(Vector<T, size> const& other);
+    GenericVector<T, size> operator/(GenericVector<T, size> const& other);
 
-    Vector<T, size> operator=(Vector<T, size> const& other);
-    void operator+=(Vector<T, size> const& other);
-    void operator-=(Vector<T, size> const& other);
-    bool operator==(Vector<T, size> const& other);
-    bool operator!=(Vector<T, size> const& other);
+    GenericVector<T, size> operator=(GenericVector<T, size> const& other);
+    void operator+=(GenericVector<T, size> const& other);
+    void operator-=(GenericVector<T, size> const& other);
+    bool operator==(GenericVector<T, size> const& other);
+    bool operator!=(GenericVector<T, size> const& other);
 };
 }
 #endif
